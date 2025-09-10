a9979fc13ad2e7ff278dbbfff3c33ac076fd83f4
Documenter.jl <documenter@juliadocs.github.io>
2025-09-10 21:24:51 +0000

    build based on d470305

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#GitHubExample","page":"Home","title":"GitHubExample","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GitHubExample.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+[{"location":"#GitHubExample","page":"Home","title":"GitHubExample","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GitHubExample.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"#GitHubExample.fibonacci-Tuple{Any}","page":"Home","title":"GitHubExample.fibonacci","text":"fibonacci(n)\n\nCalculate the nth fibonacci number, where n starts from 1.\n\n\n\n\n\n","category":"method"}]
 }
